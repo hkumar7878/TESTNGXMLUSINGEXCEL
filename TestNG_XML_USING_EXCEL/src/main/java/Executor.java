28c4f55f0f854a08b951d06d9a7abf1ba0794912
hiteshg <hiteshg@PG01CBAC.pyramidconsultinginc.com>
2019-02-14 11:48:50 +0530

    made the new changes in base class

--- TestNG_XML_USING_EXCEL/src/main/java/Executor.java
+++ TestNG_XML_USING_EXCEL/src/main/java/Executor.java
@@ -4,6 +4,8 @@ public class Executor {
 	public static void main(String[] args) {
 		XLSReader suite = new XLSReader("D:\\Project Work\\Selenium Automation Framework\\TestNG_XML_USING_EXCEL\\tests.xlsx");
 		suite.getTests("select * from TestCase where module='Order'");
+		
+		//updated
 
 	}
 
